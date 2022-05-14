a3a0b6e441492c35bab313f82729af3ff5f998c2
shafayatC <shafayatplay@gmail.com>
2022-05-14 11:08:27 +0600

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,115 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
 
-import { ExampleComponent } from 'react-scroll-effect'
+import { ScrollEffect } from 'react-scroll-effect'
 import 'react-scroll-effect/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <section>
+        <h1 className='heading_1'>Scroll to see demo ↓</h1>
+      </section>
+
+      <section>
+
+        <ScrollEffect offset = {150} duration={1} >
+          <h1 className='heading_2'>Heading Area</h1>
+
+          <div className='container'>
+            <div className='box'>
+              <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+
+          </div>
+        </ScrollEffect>
+
+      </section>
+      
+      <section>
+
+        <ScrollEffect offset = {100} duration={2} animateOut={true}>
+        <h1 className='heading_2'>Once Animate</h1>
+          <div className='container'>
+
+            <div className='box'>
+              <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+
+          </div>
+        </ScrollEffect>
+
+      </section>
+      
+      <section>
+
+        <ScrollEffect offset = {200} duration={1} >
+          <h1 className='heading_2'>Offset set 200</h1>
+
+          <div className='container'>
+            <div className='box'>
+              <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+
+          </div>
+        </ScrollEffect>
+
+      </section>
+      
+      <section>
+
+        <ScrollEffect offset = {150} duration={3} >
+          <h1 className='heading_2'>Animation duration 3 sec</h1>
+
+          <div className='container'>
+            <div className='box'>
+              <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+            <div className='box'>
+            <h3>Content Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius molestiae perferendis eos provident vitae iste.</p>
+            </div>
+
+          </div>
+        </ScrollEffect>
+
+      </section>
+      
+    </>
+
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+
+export const ScrollEffect = (props) => {
+
+  
+  const offset = props.offset ? props.offset : 150; 
+  const duration = props.duration ? props.duration+"s" : "1s"; 
+  const animateOut = props.animateOut ? props.animateOut : false; 
+
+  // unique id for each component use 
+  const uniquId = 'scrolAnim_'+ Math.floor(Math.random() * 9999)
+
+  const scrollAction = () => {
+
+     const animElem = document.querySelector(`#${uniquId}`);
+
+      
+      const windowHeight = window.innerHeight;
+      const elementTop = animElem.getBoundingClientRect().top;
+
+      console.log("uniq iD : "+ uniquId +" <> "+props.offset + " > "  + animateOut + " < "+elementTop + " window height: " + windowHeight)
+
+      if (elementTop < windowHeight - offset) {
+        animElem.classList.add(styles.active);
+      } else if(animateOut == false) {
+        animElem.classList.remove(styles.active);
+      }
+
+  }
+
+  document.addEventListener("scroll", scrollAction)
+
+  return (
+      <div id={uniquId}
+      className={props.className ? props.className + " "+styles.scrollFade : styles.scrollFade} style={{transition: duration}} >
+          {props.children}
+      </div>
+  );
+};
+
