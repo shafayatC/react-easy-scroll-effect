9abd9646de8bc51860f5bccddd1b21ddd075ba48
shafayatC <shafayatplay@gmail.com>
2022-05-14 11:37:30 +0600

    add build

---
+++ precache-manifest.a684124a61ed268463df74c1d8ff4748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3df0f0dbe3a8e5631d82aa6db21d98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcecfaa9be3c6ab0aeb9",
+    "url": "./static/css/main.3db49400.chunk.css"
+  },
+  {
+    "revision": "783842e01d36a6139075",
+    "url": "./static/js/2.a509967c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a509967c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcecfaa9be3c6ab0aeb9",
+    "url": "./static/js/main.067ed1bd.chunk.js"
+  },
+  {
+    "revision": "28ebb59e624819a003cb",
+    "url": "./static/js/runtime-main.5b9d4d35.js"
+  }
+]);
